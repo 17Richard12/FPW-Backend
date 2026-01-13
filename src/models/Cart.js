01bd9ddfc59e3c2ee635973e32089ca193077a1a
El <rafaeljovewii@gmail.com>
2026-01-13 16:09:01 +0700

    .

--- src/models/Cart.js
+++ src/models/Cart.js
@@ -1,18 +1,4 @@
-<<<<<<< HEAD
 const { default: mongoose, Schema } = require('mongoose');
-
-const cartSchema = new Schema(
-    {
-        _id: String,
-        produk_id: String,
-        jumlah: Number,
-        userId: String,
-    },
-    {
-        timestamps: true,
-    }
-=======
-const { default: mongoose, Schema } = require("mongoose");
 
 const cartSchema = new Schema(
   {
@@ -24,7 +10,6 @@ const cartSchema = new Schema(
   {
     timestamps: true,
   }
->>>>>>> ffef3f56b6626e911def7d8ec4e093196da99fec
 );
 
 const Cart = mongoose.model("Cart", cartSchema, "cart");
